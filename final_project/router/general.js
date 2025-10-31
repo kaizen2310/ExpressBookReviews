402cf7c6380f39097bdd2a6824c705d3ddd3b9ba
Shreeparth Torawane <181150650+kaizen2310@users.noreply.github.com>
2025-10-31 19:23:29 +0530

    added logic for book details based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,8 +36,26 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    const author = req.params.author;
+    const keys = Object.keys(books);
+    const matched = [];
+
+    for (let k of keys) {
+      if (books[k].author && books[k].author.toLowerCase() === author.toLowerCase()) {
+        // include isbn in the returned object
+        matched.push({ isbn: k, ...books[k] });
+      }
+    }
+
+    if (matched.length > 0) {
+      return res.status(200).json(matched);
+    } else {
+      return res.status(404).json({ message: "No books found for the given author" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving books by author" });
+  }
 });
 
 // Get all books based on title
