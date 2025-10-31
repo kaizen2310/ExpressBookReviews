8ef50d66b85c9ec50f1893cd0bff165b072592f6
Shreeparth Torawane <181150650+kaizen2310@users.noreply.github.com>
2025-10-31 19:15:39 +0530

    added book details based on ISBN logic

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,8 +21,17 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      return res.status(404).json({message: "Book not found"});
+    }
+  } catch (error) {
+    return res.status(500).json({message: "Error retrieving book details"});
+  }
  });
   
 // Get book details based on author
